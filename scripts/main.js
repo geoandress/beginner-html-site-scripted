e7d7d7ef860903964b10bf51afa8b6efee4334eb
andress.georgescu <andress.georgescu@kyndryl.com>
2022-12-01 06:59:36 +0200

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,46 @@
+
+///////// Adding an image changer ///////////////
+
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/images.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
+/////////////////////////////////////////////////////
+
+
+
+//////// Adding a personalized welcome message //////////
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+  
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
+
+////////////////////////////////////////////////////////////////
